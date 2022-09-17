c9a7bbf20f7e49c670d74a379087c52d174bd64c
bob1996w <bob1996w@gmail.com>
2022-09-17 21:15:09 +0800

    Add copyright
    Test patch for adding copyright

--- keymaps/bob1996w/config.h
+++ keymaps/bob1996w/config.h
@@ -1,6 +1,7 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
+Copyright 2022 bob1996w <@bob1996w>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
